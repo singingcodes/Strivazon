480ff7c6190ee2b26d524945761d44f2ded8cf98
Sarah Ayanwale <sarahayanwale@gmail.com>
2022-04-14 18:30:24 +0200

    added put and delete function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const id = new URLSearchParams(window.location.search).get("ProductId")
+let id = new URLSearchParams(window.location.search).get("id")
 console.log(id)
 const url = id? "https://striveschool-api.herokuapp.com/api/product/" + id : "https://striveschool-api.herokuapp.com/api/product/"
 
@@ -37,7 +37,7 @@ const addProducts= async (event)=>{
     if(id){
         console.log(id)
         const response = await fetch("https://striveschool-api.herokuapp.com/api/product/" + id, {
-            method: "PUT",
+           
             headers: {
                 
                 "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmQxY2E5MDIzOTAwMTVkOTY1YzciLCJpYXQiOjE2NDk4NTE2NzcsImV4cCI6MTY1MTA2MTI3N30.vQNUcd502n11trxsE51poL09iufcBbRkpz7Y7Ojq7l0"
@@ -59,26 +59,35 @@ const addProducts= async (event)=>{
    
    } 
 
-   const handleDelete = async () => {
-    const confirmed = confirm("Are you sure you want to delete this?")
-    if (confirmed) {
-        try {
-            const response = await fetch(url, {
-                method:"DELETE",
-                headers: {
-                'Content-Type': 'application/json',
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmQxY2E5MDIzOTAwMTVkOTY1YzciLCJpYXQiOjE2NDk4NTE2NzcsImV4cCI6MTY1MTA2MTI3N30.vQNUcd502n11trxsE51poL09iufcBbRkpz7Y7Ojq7l0"
-                }})
-            if (response.ok) {
-                console.log(response)
-                alert( "Product deleted successfully")
-                setTimeout(() => { window.location.assign("/") }, 4100)
-            } else {
-                alert("Something went wrong in the deletion process")
-            }
-        } catch (err) {
-            alert( err.message)
-        }
+   const handleSubmit = async(event)=>{
+    event.preventDefault()
+     
+    let myProducts ={
+        name: document.getElementById("name").value,
+        description: document.getElementById("description").value,
+        brand: document.getElementById("brand").value,
+        price: document.getElementById("price").value,
+        imageUrl: document.getElementById("image").value
     }
-}
+    event.preventDefault()
+    const response = await fetch(url, {
+        method:"PUT",
+        headers: {
+        'Content-Type': 'application/json',
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmQxY2E5MDIzOTAwMTVkOTY1YzciLCJpYXQiOjE2NDk4NTE2NzcsImV4cCI6MTY1MTA2MTI3N30.vQNUcd502n11trxsE51poL09iufcBbRkpz7Y7Ojq7l0"
+        },
+        body: JSON.stringify(myProducts)
+        })
+        if(response.ok){
+            const body = await response.json()
+            console.log(body)
+            if (id) {
+                alert( "Appointment with an id of: " + body._id + " got edited successfully")
+                window.location.assign("./index.html")
+            } else {
+                alert( "Appointment created successfully with an id of " + body._id)
+            }
+        }
+   }
 
+  
